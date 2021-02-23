66b5b7e3a880d003b4fd50b3dae406e01696267e
Realgrapejuice <66715905+realgrapejuice@users.noreply.github.com>
2021-02-23 17:14:02 +0900

    add forms

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Youtube Clone
 
 Youtube Clone with vanilaJS and nodeJS
+
+## Pages:
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload Video
+- [ ] Video Detail
+- [ ] Edit Video
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,23 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.send("Search");
-export const videos = (req, res) => res.send("/Videos");
-export const upload = (req, res) => res.send("/Upload");
-export const videoDetail = (req, res) => res.send("/VideoDetail");
-export const editVideo = (req, res) => res.send("/edit Video");
-export const deleteVideo = (req, res) => res.send("/delete video");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy },
+  } = req;
+  return res.render("search", { pageTitle: "Search", searchingBy });
+};
+
+export const videos = (req, res) =>
+  res.render("videos", { pageTitle: "Videos" });
+
+export const upload = (req, res) =>
+  res.render("upload", { pageTitle: "Upload" });
+
+export const videoDetail = (req, res) =>
+  res.render("videoDetail", { pageTitle: "Video Detail" });
+
+export const editVideo = (req, res) =>
+  res.render("editVideo", { pageTitle: "Edit Video" });
+
+export const deleteVideo = (req, res) =>
+  res.render("deleteVideo", { pageTitle: "Delete Video" });
